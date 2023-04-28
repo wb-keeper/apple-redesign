52c797b23fb8c19d50a50a70e80549b1a2144012
wb-keeper <wb-keeper@mail.ru>
2023-04-28 12:04:21 +0300

    First commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["rb.gy", "images.unsplash.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
